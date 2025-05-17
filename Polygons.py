ba1169cd30a2f8d89805313f31aab0175ff36568
Vilokyacoding <vilokya2025@gmail.com>
2025-05-16 21:43:56 -0400

    Polygons

---
+++ Polygons.py
@@ -0,0 +1,20 @@
+import turtle
+turtle.Screen().bgcolor("pink")
+sc=turtle.Screen()
+sc.setup(400,300)
+turtle.title("Welcome to turtle window")
+board=turtle.Turtle()
+# Triangle
+board.forward(100)
+board.left(120)
+board.forward(100)
+board.left(120)
+board.forward(100)
+
+# Square
+board.forward(100)
+board.left(90)
+board.forward(100)
+board.left(90)
+board.forward(100)
+board.left(90)
