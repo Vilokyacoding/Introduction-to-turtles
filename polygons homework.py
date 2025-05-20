c85569edf4ac69ac0f9c4e8f0f784e7c1c5827b8
Vilokyacoding <vilokya2025@gmail.com>
2025-05-19 20:42:40 -0400

    polygonshomework

---
+++ polygons homework.py
@@ -0,0 +1,27 @@
+import turtle
+turtle.Screen().bgcolor("pink")
+sc=turtle.Screen()
+sc.setup(400,300)
+turtle.title("Welcome to turtle window")
+board=turtle.Turtle()
+# Pentagon
+board.forward(100)  
+board.left(72)      
+board.forward(100)
+board.left(72)
+board.forward(100)
+board.left(72)
+board.forward(100)
+board.left(72)
+board.forward(100)
+
+
+# Rectangle
+board.forward(100)
+board.left(90)
+board.forward(50)
+board.left(90)
+board.forward(100)
+board.left(90)
+board.forward(50)   
+board.left(90)  
